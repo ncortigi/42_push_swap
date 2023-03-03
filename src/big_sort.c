5f1affd2abd01e2175fa76613ed963917839e0e5
Neri Cortigiani <ncortigi@c2r2p13.42firenze.it>
2023-03-03 18:10:31 +0100

    Si_blocca_da_qualche_parte

--- src/big_sort.c
+++ src/big_sort.c
@@ -6,11 +6,44 @@
 /*   By: ncortigi <ncortigi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/22 17:21:32 by ncortigi          #+#    #+#             */
-/*   Updated: 2023/03/02 17:54:36 by ncortigi         ###   ########.fr       */
+/*   Updated: 2023/03/03 18:05:59 by ncortigi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+
+t_stacks	*find_key(t_stacks *stack, int key)
+{
+	t_stacks	*found;
+
+	found = stack;
+	while (stack)
+	{
+		if (found->i <= key)
+			break ;
+		stack = stack->next;
+		found = stack;
+	}
+	return (found);
+}
+
+void	make_clever_push_b(t_stacks **stack_a, t_stacks **stack_b, int size, t_stacks *num)
+{
+	ft_printf("%d", num->i);
+	if (find_position(*stack_a, num) < (size / 2))
+	{
+		while ((*stack_a)->i != num->i)
+			rotate(stack_a, 'a');
+	}
+	else if (find_position(*stack_a, num) == 1)
+		swap(*stack_a, 'a');
+	else
+	{
+		while ((*stack_a)->i != num->i)
+			r_rotate(stack_a, 'a');
+	}
+	push(stack_a, stack_b, 'b');
+}
 
 void	big_sort(t_stacks **stack_a, t_stacks **stack_b, int size)
 {
@@ -22,13 +55,14 @@ void	big_sort(t_stacks **stack_a, t_stacks **stack_b, int size)
 	else
 		chunks = 8;
 	key = size / chunks;
+	//ft_printf("%d", key);
 	while (!check_sort(*stack_a))
 	{
-		while ((chunks - 1) != 0)
+		while ((chunks) != 1)
 		{
 			while (calc_size(*stack_b) < key)
 			{
-				make_clever_push_b(stack_a, stack_b, size);
+				make_clever_push_b(stack_a, stack_b, size, find_key(*stack_a, key));
 				size = calc_size(*stack_a);
 			}
 			size = calc_size(*stack_a);
